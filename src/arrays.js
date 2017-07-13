ecf5863661090eaf0a0925112882b1f8021a9ef6
Allan Felipe <allan.murara@gmail.com>
2017-07-13 17:43:18 -0300

    Added arrays files

--- src/arrays.js
+++ src/arrays.js
@@ -6,33 +6,71 @@ const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   // based off http://underscorejs.org/#each
+  elements.forEach((t, i) => {
+    cb(t, i);
+  });
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newarray = [];
+  elements.forEach((t) => {
+    newarray.push(cb(t));
+  });
+  return newarray;
 };
 
-const reduce = (elements, cb, memo) => {
+const reduce = (elements, cb, memo = elements[0]) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+  //
+  // should iteerate over all elements
+  if (memo === elements[0]) elements.shift();
+  elements.forEach((t) => {
+    memo = cb(memo, t);
+  });
+  return memo;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  let result;
+  elements.forEach((t) => {
+    if (cb(t) === true) {
+      result = t;
+      return;
+    }
+  });
+  return result;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const result = [];
+  elements.forEach((t) => {
+    if (cb(t) === true) {
+      result.push(t);
+    }
+  });
+  return result;
 };
 
 const flatten = (elements) => {
-  // Flattens a nested array (the nesting can be to any depth).
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  // let set = new Set(arr);
+  const arreturn = [];
+  elements.forEach((t) => {
+    let element = t;
+    while (typeof element === 'object') {
+      element = element[0];
+    }
+    arreturn.push(element);
+  });
+  return arreturn;
 };
 
 /* eslint-enable no-unused-vars, max-len */
